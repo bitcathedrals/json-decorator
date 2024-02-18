ee7e2cd03e5d0637d8a8c5bb2760702a4284de41
Michael Mattie <codermattie@runbox.com>
2024-02-18 11:32:43 -0800

    (fix) change-over to the new pythonsh scheme

--- python.sh
+++ python.sh
@@ -1,8 +1,17 @@
-# pythonsh configuration file
+# json-decorator - python.sh
 VERSION=0.5.0
 
-VIRTUAL_PREFIX='json-decorator'
 PYTHON_VERSION='3.12'
+SOURCE="src"
+PACKAGES="json_decorator"
+
+BUILD_NAME="json-decorator"
+
+VIRTUAL_PREFIX='json-decorator'
+
+
+
+
 
 
 
